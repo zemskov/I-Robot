509741123037020a0f5cb53f69eb63c87947e713
Alexey Zemskov <a.zemskov@gmail.com>
2015-04-23 03:42:12 +0300

    ok, robot takes rest commands

--- I-Robot/src/frolov/robot/rest/scratch/FEConnector.java
+++ I-Robot/src/frolov/robot/rest/scratch/FEConnector.java
@@ -4,16 +4,40 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.springframework.context.*;
+import org.springframework.context.support.*;
+import frolov.robot.*;
 
 
 
 @Path("/")
 public class FEConnector{
+   
+   final RoboConfig roboConfig;
+   
+   public FEConnector(){
+      ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+      roboConfig = ctx.getBean("robo_config", RoboConfig.class);       
+   }
 
    @GET
-   @Path("/test")
+   @Path("/forward")
    @Produces(MediaType.TEXT_PLAIN)
-   public String test(){
+   public String forward() throws Exception{
+      roboConfig.mapCommands.get("Ехать вперед").run();
+      
       return "Test";
    }
+   
+   
+   
+   @GET
+   @Path("/stop")
+   @Produces(MediaType.TEXT_PLAIN)
+   public String stop() throws Exception{
+      roboConfig.mapCommands.get("Стоп!").run();
+      
+      return "Test";
+   }
+   
 }
