2992c531cf8e0785d92ba58552982961405f2645
Alexey Zemskov <a.zemskov@gmail.com>
2015-04-23 04:11:26 +0300

    Tray icon

--- I-Robot/src/frolov/robot/Main.java
+++ I-Robot/src/frolov/robot/Main.java
@@ -1,6 +1,8 @@
 package frolov.robot;
 
 
+import java.awt.*;
+import java.awt.event.*;
 import java.net.*;
 import java.util.*;
 import javax.swing.*;
@@ -132,6 +134,78 @@ public class Main extends JFrame{
                                       null, 
                                       new String[]{"Начать работу", "Диагностика"}, // this is the array
                                       "default") == JOptionPane.YES_OPTION){
+         
+         
+         final TrayIcon trayIcon;
+         
+         if (SystemTray.isSupported()) {
+          
+             SystemTray tray = SystemTray.getSystemTray();
+             Image image = Toolkit.getDefaultToolkit().getImage(JFrame.class.getResource("/loaderB32.gif"));
+          
+             MouseListener mouseListener = new MouseListener() {
+                          
+                 public void mouseClicked(MouseEvent e) {
+                     System.out.println("Tray Icon - Mouse clicked!");                
+                 }
+          
+                 public void mouseEntered(MouseEvent e) {
+                     System.out.println("Tray Icon - Mouse entered!");                
+                 }
+          
+                 public void mouseExited(MouseEvent e) {
+                     System.out.println("Tray Icon - Mouse exited!");                
+                 }
+          
+                 public void mousePressed(MouseEvent e) {
+                     System.out.println("Tray Icon - Mouse pressed!");                
+                 }
+          
+                 public void mouseReleased(MouseEvent e) {
+                     System.out.println("Tray Icon - Mouse released!");                
+                 }
+             };
+          
+             ActionListener exitListener = new ActionListener() {
+                 public void actionPerformed(ActionEvent e) {
+                     System.out.println("Exiting...");
+                     System.exit(0);
+                 }
+             };
+                      
+             PopupMenu popup = new PopupMenu();
+             MenuItem defaultItem = new MenuItem("Exit");
+             defaultItem.addActionListener(exitListener);
+             popup.add(defaultItem);
+          
+             trayIcon = new TrayIcon(image, "Tray Demo", popup);
+          
+             ActionListener actionListener = new ActionListener() {
+                 public void actionPerformed(ActionEvent e) {
+                     trayIcon.displayMessage("Action Event",
+                         "An Action Event Has Been Performed!",
+                         TrayIcon.MessageType.INFO);
+                 }
+             };
+                      
+             trayIcon.setImageAutoSize(true);
+             trayIcon.addActionListener(actionListener);
+             trayIcon.addMouseListener(mouseListener);
+          
+             try {
+                 tray.add(trayIcon);
+             } catch (AWTException e) {
+                 System.err.println("TrayIcon could not be added.");
+             }
+          
+         } else {
+          
+             //  System Tray is not supported
+          
+         }      
+         
+         
+         
          
          ctx.getBean("rest", IRest.class).start();       
       }
