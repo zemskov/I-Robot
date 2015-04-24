18771e8405df78ff4d4436e7e7db344a6a2b2a63
Alexey Zemskov <a.zemskov@gmail.com>
2015-04-24 06:50:26 +0300

    crossdomain.xml

--- I-Robot/src/frolov/robot/rest/scratch/FEConnector.java
+++ I-Robot/src/frolov/robot/rest/scratch/FEConnector.java
@@ -1,18 +1,37 @@
 package frolov.robot.rest.scratch;
 
 import javax.servlet.*;
+import javax.servlet.http.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import org.apache.commons.logging.*;
 import frolov.robot.*;
 
 
 
 @Path("/")
 public class FEConnector{
+   private static Log log = LogFactory.getLog(FEConnector.class);
+   private static final String LOG = "[Connector] ";
+   
+   
    @Context ServletContext context;   
    
-   public FEConnector(){
+   public FEConnector(@Context HttpServletRequest hsr){
+      log.info(LOG + "\n------------------------------------\n" + hsr.getRequestURI());
+      
    }
+   
+   
+   @GET
+   @Path("/crossdomain.xml")
+   @Produces(MediaType.APPLICATION_XML)
+   public String crossdomain() throws Exception{
+      return "<?xml version=\"1.0\"?><cross-domain-policy><allow-access-from domain=\"*\" /></cross-domain-policy>";
+   }
+   
+   
+   
 
    @GET
    @Path("/forward")
