eb503ae9b34c5a3db2f57745ec65680614c433df
Alexey Zemskov <a.zemskov@gmail.com>
2015-08-08 00:12:50 +0300

    rrr

---
+++ I-Robot/src/scratchduino/robot/RoboConfig.java
@@ -0,0 +1,17 @@
+package scratchduino.robot;
+
+import java.util.*;
+
+
+public class RoboConfig{
+ 
+   public final IPortDetector portDetecter;
+   public final Map<String, ICommand> mapCommands;
+
+
+   public RoboConfig(IPortDetector portDetecter, Map<String, ICommand> mapCommands){
+      this.portDetecter = portDetecter;
+      this.mapCommands = mapCommands;
+   }
+}
+
